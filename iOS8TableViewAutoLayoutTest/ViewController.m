a1d9e3c9e634145e9887a1fcc3a04633515ec762
John Stewart <jstewart@jstewart-mba.local>
2015-08-12 23:45:54 -0500

    Added     self.tableView.rowHeight = UITableViewAutomaticDimension;

--- iOS8TableViewAutoLayoutTest/ViewController.m
+++ iOS8TableViewAutoLayoutTest/ViewController.m
@@ -17,6 +17,16 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    // Self-sizing table view cells in iOS 8 require that the rowHeight property of the table view be set to the constant UITableViewAutomaticDimension
+    self.tableView.rowHeight = UITableViewAutomaticDimension;
+    
+    // Self-sizing table view cells in iOS 8 are enabled when the estimatedRowHeight property of the table view is set to a non-zero value.
+    // Setting the estimated row height prevents the table view from calling tableView:heightForRowAtIndexPath: for every row in the table on first load;
+    // it will only be called as cells are about to scroll onscreen. This is a major performance optimization.
+    self.tableView.estimatedRowHeight = 44.0; // set this to whatever your "average" cell height is; it doesn't need to be very accurate
+
 }
 
 - (void)didReceiveMemoryWarning {
