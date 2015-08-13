6580a7e6b4a4c4e004b7058fd81945c3ac820c67
John Stewart <jstewart@jstewart-mba.local>
2015-08-12 23:40:39 -0500

    Initial Commit

---
+++ iOS8TableViewAutoLayoutTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS8TableViewAutoLayoutTest
+//
+//  Created by John Stewart on 8/12/15.
+//  Copyright © 2015 John Stewart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
