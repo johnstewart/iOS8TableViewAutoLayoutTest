f6af8d6de369157d8ef24e682ab2cf00b8055b48
John Stewart <jstewart@jstewart-mba.local>
2015-08-14 10:40:03 -0500

    Reproduced with dynamic tableviewcell, works fine.

---
+++ iOS8TableViewAutoLayoutTest/MyDynamicTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyDynamicTableViewController.h
+//  iOS8TableViewAutoLayoutTest
+//
+//  Created by John Stewart on 8/14/15.
+//  Copyright © 2015 John Stewart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyDynamicTableViewController : UITableViewController
+
+@end
